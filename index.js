ac24c8b83b5205e1a77155ee7ec01df940e417bf
Renata Nunes <renatanunes231@yahoo.com.br>
2021-10-27 16:41:06 -0300

    Requisito 1 - Get All Talkers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,10 +8,37 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkers = './talker.json';
+
+// -----------------------------------------------------------------------------------------
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// -----------------------------------------------------------------------------------------
+
+async function getTalkers() {
+  try {
+    const result = await fs.readFile(talkers, 'utf-8');
+    const data = JSON.parse(result);
+    return data;
+  } catch (err) {
+    console.log(`Erro ao ler o arquivo: ${err.path}`);
+  }
+}
+
+app.get('/talker', async (_req, res) => {
+  const data = await getTalkers();
+  
+  if (data) {
+    return res.status(200).json(data);
+  }
+
+  return res.status(200).json([]);
+});
+
+// -----------------------------------------------------------------------------------------
 
 app.listen(PORT, () => {
   console.log('Online');
